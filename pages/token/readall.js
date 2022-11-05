9ce4df413ddea9c105de837a6bd1958d70e65a4a
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2022-11-05 10:09:42 +0900

    domain

--- pages/token/readall.js
+++ pages/token/readall.js
@@ -42,7 +42,7 @@ const ReadAllTokens = (props) => {
 export default ReadAllTokens
 
 export const getStaticProps = async() => {
-    const response = await fetch("http://localhost:3000/api/token/readall")   
+    const response = await fetch("https://tag-dao-project-hack-akiyoshi-yagi.vercel.app/api/token/readall")   
     const allTokens = await response.json() 
     console.log(allTokens);
 
